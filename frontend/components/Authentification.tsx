29c437c2f308b178ad4b1c9a3052f57aefd11251
halias3422 <v.desaintexupery@gmail.com>
2022-05-17 20:21:48 +0200

    added out of div click disappearance for signin popup + overflow temporarely hidden

--- frontend/components/Authentification.tsx
+++ frontend/components/Authentification.tsx
@@ -1,24 +1,39 @@
 import Image from 'next/image';
-import { Dispatch, SetStateAction, useCallback, useEffect, useState } from 'react';
-import { handleUserConnection, handleUserRegistration } from '../dbLogic/userRegistration';
+import { Dispatch, SetStateAction, useCallback, useEffect, useRef, useState } from 'react';
+import { handleUserConnection, handleUserRegistration } from '../dbLogic/userRecognition';
 import styles from '../styles/Authentification.module.css'
 
 function Authentification({ signingUp, shown, setShown }: { signingUp: boolean, shown: boolean, setShown: Dispatch<SetStateAction<boolean>> }) {
     let contextText: string = "Sign Up";
+    let clickCount: number = 0;
     if (!signingUp) {
         contextText = "Sign In";
     }
-
+    document.body.style.overflow = 'hidden';
     const [errorSignUp, setErrorSignUp] = useState(false);
     const [errorSignIn, setErrorSignIn] = useState(false);
+    const popUp = useRef<HTMLDivElement>(null);
 
     const toggleVisibility = useCallback (() => {
+        document.body.style.overflow = 'auto';
             setShown(false);
     }, [setShown]);
 
+    useEffect(() => {
+        function handleClick(event: MouseEvent) {
+            // CORRECT CLICKCOUNT FOR PRODUCTION (SET TO 2 BECAUSE OF REACTSTRICTCODE)
+            if (clickCount >= 2 && shown && popUp.current && !popUp.current.contains(event.target as Node)) {
+                document.body.style.overflow = 'auto';
+                setShown(false);
+            }
+            clickCount += 1;
+        }
+        window.addEventListener("click", handleClick);
+    })
+
     return (
         <div className={styles.globalContainer} >
-            <div className={styles.iframe}>
+            <div ref={popUp} className={styles.iframe}>
                 <div className={styles.closeContainer}>
                     <Image className={styles.closeIcon} src='/close_icon.png' alt='close' width={60} height={60} onClick={toggleVisibility}/>
                 </div>
